8d73e76241e85c6f625b9d6535e47a272d9e8bca
jaehui327 <jaehui327@naver.com>
2019-07-02 21:11:03 +0900

    Update AddRestaurantViewController.swift

--- MapOfFamousRestaurant/MapOfFamousRestaurant/view controllers/AddRestaurantViewController.swift
+++ MapOfFamousRestaurant/MapOfFamousRestaurant/view controllers/AddRestaurantViewController.swift
@@ -2,8 +2,11 @@ import UIKit
 
 class AddRestaurantViewController: UIViewController {
     
+    // MARK: - properties
+    // 장소 서비스 모델
     var locaionModel: LocationService?
     
+    // MARK: - IBOutlet
     @IBOutlet weak var longitudeTextField: UITextField!
     @IBOutlet weak var latitudeTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
@@ -12,17 +15,26 @@ class AddRestaurantViewController: UIViewController {
     
     @IBOutlet weak var sendButton: UIButton!
     
+    // MARK: - IBAction
+    // 장소 등록 버튼 탭 했을 때 - post
     @IBAction func tappedSend(_ sender: Any) {
+        // textfield 값 Double형으로 변환
         guard let longitude = NumberFormatter().number(from: longitudeTextField.text ?? "")?.doubleValue else { return }
         guard let latitude = NumberFormatter().number(from: latitudeTextField.text ?? "")?.doubleValue else { return }
+        
+        // network
         locaionModel?.postLocationInformation(longitude: longitude, latitude: latitude, name: nameTextField.text ?? "", description: descriptionTextField.text ?? "", address: addressTextField.text ?? "") { locationList in
+            // 전송 완료 log
+            print("postLocationIformation success - longitude : \(longitude), latitude: \(latitude), name: \(String(describing: self.nameTextField.text)), description: \(String(describing: self.descriptionTextField.text)), address: \(String(describing: self.addressTextField.text))")
             self.navigationController?.popViewController(animated: true)
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    // MARK: - life cycle
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        // 장소 모델 대입
         locaionModel = LocationService()
         
         //빈화면 탭했을 때 제스처 추가
